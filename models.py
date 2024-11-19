d5730bf213b911409da112b010c4ac1d9425d8d0
Zachary Messer <zmesser@ucsc.edu>
2024-11-18 19:13:49 -0800

    Add table definitions

--- models.py
+++ models.py
@@ -8,16 +8,30 @@ from pydal.validators import *
 
 
 def get_user_email():
-    return auth.current_user.get('email') if auth.current_user else None
+    return auth.current_user.get("email") if auth.current_user else None
+
 
 def get_time():
     return datetime.datetime.utcnow()
 
 
-### Define your table below
-#
-# db.define_table('thing', Field('name'))
-#
-## always commit your models to avoid problems later
+db.define_table(
+    "sighting",
+    Field("event_id"),
+    Field("common_name"),
+    Field("count", "integer"),
+)
+
+db.define_table(
+    "checklist",
+    Field("event_id"),
+    Field("latitude", "double"),
+    Field("longitude", "double"),
+    Field("date", "datetime"),
+    Field("observer_id", "reference auth_user"),
+    Field("duration_minutes", "integer"),
+)
+
+db.define_table("species", Field("name", unique=True))
 
 db.commit()
