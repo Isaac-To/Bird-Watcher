13158aa045076dac0b2893560705e4cf85974035
Zachary Messer <zmesser@ucsc.edu>
2024-12-12 00:45:38 -0800

    Automatically fill empty databases on launch

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ This file defines the database models
 """
 
 import datetime
-from .common import db, Field, auth
+import csv
+from .common import db, Field, auth, logger
 from pydal.validators import *
 
 
@@ -28,10 +29,52 @@ db.define_table(
     Field("latitude", "double"),
     Field("longitude", "double"),
     Field("date", "datetime"),
-    Field("observer_id", "reference auth_user"),
+    Field("observer_id"),
     Field("duration_minutes", "integer"),
 )
 
 db.define_table("species", Field("name", unique=True))
+
+# Prime databases
+if db(db.sighting).isempty():
+    print("Priming sighting database...")
+    sightings = csv.reader(open("sample_data/sightings.csv"), delimiter=",")
+
+    db.sighting.bulk_insert(
+        [
+            dict(
+                event_id=sighting[0][1:],
+                common_name=sighting[1],
+                count=1 if sighting[2] == "X" else sighting[2],
+            )
+            for sighting in list(sightings)[1:]
+        ]
+    )
+
+if db(db.checklist).isempty():
+    print("Priming checklist database...")
+    checklist = csv.reader(open("sample_data/checklists.csv"), delimiter=",")
+
+    db.checklist.bulk_insert(
+        [
+            dict(
+                event_id=event[0][1:],
+                latitude=float(event[1]),
+                longitude=float(event[2]),
+                date=datetime.datetime.fromisoformat(
+                    event[3] + " " + (event[4] or "12:00:00")
+                ),
+                observer_id=event[5][3:],
+                duration_minutes=None if event[6] == "" else int(float(event[6])),
+            )
+            for event in list(checklist)[1:]
+        ]
+    )
+
+if db(db.species).isempty():
+    print("Priming species database...")
+    species = csv.reader(open("sample_data/species.csv"), delimiter=",")
+
+    db.species.bulk_insert([dict(name=bird[0]) for bird in list(species)[1:]])
 
 db.commit()
